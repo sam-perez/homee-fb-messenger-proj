c4f7f754767738f19b1316474c53a9e1c88f1354
Sam Perez <samuel.e.perez@gmail.com>
2016-05-29 23:47:11 +0000

    Add persistent store for conversations
    Added a module that exposes an api for persistently storing our
    conversations. We are using the file system with our current
    implementation.

---
+++ conversationStateStore.js
@@ -0,0 +1,121 @@
+/********
+Provide a module that will store the state of our message bots.
+This module returns an object that exposes a simple API (can be
+swapped out by a mock for testing purposes):
+
+getConversationState(facebookId): an observable that emits the conversation
+state then completes
+
+saveConversationState(facebookId, conversationState): stores the conversation
+state in some persistant storage
+********/
+
+const fs = require('fs');
+const Immutable = require('immutable');
+const path = require('path');
+const Rx = require('rx');
+
+const createFileSystemConversationStateStore = () => {
+  // helpers for interacting with the fs
+  // Asynchronously reads the file at the path.
+  const readFile = (path, encoding) => {
+
+  	if(encoding === undefined) {
+  	    encoding = 'utf-8';
+  	}
+
+  	return Rx.Observable.create((observer) => {
+  		fs.readFile(path, encoding, (e, file) => {
+  			if(e) {
+  			    return observer.onError(e);
+		    };
+
+  			observer.onNext(file);
+  			observer.onCompleted();
+  		});
+  	});
+  };
+
+  // Writes the file to the path.
+  // We assume that the path to the file exists
+  const writeFile = (path, data) => {
+  	return Rx.Observable.create(function(observer){
+  		fs.writeFile(path, data, (e) => {
+  			if(e) {
+			    return observer.onError(e);
+  			}
+
+  			observer.onNext();
+  			observer.onCompleted();
+  		});
+  	});
+  };
+
+  const getConversationPath = (facebookId) => {
+    // place the store next to this file
+    return path.join(__dirname, 'conversationStore', facebookId);
+  };
+
+  // Version of a conversation state store which persists
+  // the conversations to disk
+
+  const getDefaultConversationState = (facebookId) => {
+      return Immutable.fromJS({
+        facebookId: facebookId,
+        answers: []
+      });
+  };
+
+  // an internal cache of conversation states.
+  // this should be used by default, but it will
+  // only contain a value for a facebookId when the
+  // data is being persisted to disk
+  const cachedConversationStates = {};
+
+  return {
+    getConversationState: (facebookId) => {
+        if (cachedConversationStates[facebookId] !== undefined) {
+          // just return our cached result
+          return Rx.Observable.return(
+              cachedConversationStates[facebookId]
+          );
+        } else {
+          const conversationFileName = getConversationPath(facebookId);
+
+          const defaultConversationStateObs = Rx.Observable.return(
+            getDefaultConversationState(facebookId)
+          );
+
+          const fromFileObs = readFile(conversationFileName)
+            .retry(3)
+            .map((fileData) => {
+                return Immutable.fromJS(JSON.parse(fileData));
+            });
+
+          // catch both file read and json parse errors
+          return Rx.Observable.catch(
+            fromFileObs,
+            defaultConversationStateObs
+          );
+        }
+      },
+
+      saveConversationState: (facebookId, conversationState) => {
+        cachedConversationStates[facebookId] = conversationState;
+
+        const conversationFileName = getConversationPath(facebookId);
+
+        writeFile(conversationFileName, JSON.stringify(conversationState))
+          .retry(3)
+          .finally(() => {
+             // empty the cache
+             delete conversationFileName[facebookId];
+          })
+          // subscribe to the observable to kick things off
+          .subscribe();
+      }
+  };
+}
+
+exports.createFileSystemConversationStateStore =
+    createFileSystemConversationStateStore;
--- index.js
+++ index.js
@@ -1,10 +1,64 @@
+const FB_MESSENGER_ACCESS_TOKEN = 'EAAWpbdicWE0BAPHsct3gxpJEOAMvHfDkAXPNlBc2WXLOPgRZBlGp9QmHkIt72DHH2JBxF8T75VNsbDIXid1sgpLZASqHIM2CUqxg12uJOrURjq1iV6L4qfZCpDzU5ZAMGKtbWkJCAl1uO6MQiFr4YOGdTZBQa2D8afODEaK3JVAZDZD';
+const FB_MESSENGER_VERIFY_TOKEN = '0a30d0b17b8349d299e98d244c67795e';
+
+const bodyParser = require('body-parser');
 const http = require('http');
 const express = require('express');
+const request = require('request');
 
 const router = express();
+router.use(bodyParser.json()); // for parsing application/json
+
 const server = http.createServer(router);
+
 router.get('/test', function(req, res){
   res.send('hello world');
+});
+
+router.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === FB_MESSENGER_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
+const sendTextMessage = (sender, text) => {
+  const messageData = {
+    text
+  };
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:FB_MESSENGER_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, (error, response, body) => {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+router.post('/webhook/', function (req, res) {
+  const messaging_events = req.body.entry[0].messaging;
+  for (var i = 0; i < messaging_events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      // Handle a text message from this sender
+      console.log('Received a message: ' + text);
+
+      sendTextMessage(sender, text);
+    }
+  }
+  res.sendStatus(200);
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
